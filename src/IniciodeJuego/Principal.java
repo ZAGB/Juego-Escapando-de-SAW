523527bda53bbb331ee06b73edf83005016168d4
Alexis23l <102411449+alexisSandovalC23071989@users.noreply.github.com>
2022-07-09 01:13:17 -0600

    Avance

---
+++ src/IniciodeJuego/Principal.java
@@ -0,0 +1,22 @@
+
+package IniciodeJuego;
+
+/**
+ *
+ * @author aldav
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        Nivel_Intermedio Nuevo = new Nivel_Intermedio();
+        
+        Nuevo.setVisible(true);
+         
+        
+    }
+    
+}
